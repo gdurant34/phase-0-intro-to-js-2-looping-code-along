b4b213e0e686fa4edfe370b7bbef750db62d3fca
Grace Durant <gdurant34@gmail.com>
2022-09-25 23:56:23 -0700

    commit tree 6e99088e07769ec8cf57ec19ad3bac560da13631

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let newArr = [];
+    for(let i = 0; i < names.length; i++) {
+        let name = names[i];
+        // console.log(name, event);
+        let msg = `Thank you, ${name}, for the wonderful ${event} gift!`
+        newArr.push(msg);
+        // console.log(msg);
+    }
+    // console.log(newArr);
+    return newArr;
+};
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i--);
+    }
+    // i--;
+}
+
+countDown(10);
